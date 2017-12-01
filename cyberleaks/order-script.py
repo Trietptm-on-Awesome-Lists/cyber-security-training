59ce9b9c6b0c49e1b56d4cb92246b764250ab6bb
Onni Hakkari <onni.hakkari@gmail.com>
2017-12-01 15:12:28 +0200

    Update order-script

--- cyberleaks/order-script.py
+++ cyberleaks/order-script.py
@@ -1,13 +1,16 @@
 import requests
 import time
 
-home_url = "www.webbikauppa.fi"
+home_url = "http://www.webbikauppa.fi"
 cart_url = "{}/index.php/ostoskori".format(home_url)
 
 while 1:
     time.sleep(0.5)
     try:
+        print("Ordering")
         requests.get(cart_url)
         requests.get(home_url)
-    except:
+        print("Done")
+    except Exception as e:
+        print(e)
         pass
